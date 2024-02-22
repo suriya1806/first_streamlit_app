ae046011d748a09e613ad1414f208d6bc0393142
suriya1806 <160858156+suriya1806@users.noreply.github.com>
2024-02-22 18:50:30 +0530

    Update streamlit_app.py
    added breakfasat menu without numbers

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My parents New Healthy Diner')
 
 streamlit.header('Breakfast menu')
-streamlit.text('1. Omega 3 and Blueberry oat meal')
-streamlit.text('2. Kale, Spinach & Rocket Smoothie')
-streamlit.text('3. Hard-Bolied  Free Range Egg')
+streamlit.text('Omega 3 and Blueberry oat meal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Bolied  Free Range Egg')
